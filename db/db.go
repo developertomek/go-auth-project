9574ce091dca8a75ad50da1145a93d867130c047
developertomek <tomekcodes@gmail.com>
2024-09-29 19:13:15 +0200

    add sqlite

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Open() (*sql.DB, error) {
+	sqlFile, err := os.ReadFile("sql/users.sql")
+
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("sqlite3", ":memory")
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(string(sqlFile))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/developertomek/go-auth-project
 go 1.22.2
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require github.com/mattn/go-sqlite3 v1.14.23 // indirect
