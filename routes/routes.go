d5181ba582824af86145d6c602b596d0f0bb609b
developertomek <tomekcodes@gmail.com>
2024-10-03 19:44:54 +0200

    add middleware

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,11 @@
 package routes
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/developertomek/go-auth-project/api"
+	"github.com/developertomek/go-auth-project/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -11,5 +15,14 @@ func SetupRoutes(userHandler api.UserHandler) *chi.Mux {
 	r.Post("/echo", api.HandleEchoUser)
 	r.Post("/register", userHandler.HandlerRegisterUser)
 	r.Post("/login", userHandler.HandlerLoginUser)
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Use(middleware.UserAuthentication)
+		r.Get("/test", HandleTest)
+	})
 	return r
+}
+
+func HandleTest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "test working")
 }
